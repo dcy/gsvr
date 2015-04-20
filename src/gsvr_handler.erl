549c2cd40c1fc045f1623244a9e2cce9630c1bdd
dcy <dcy.dengcaiyun@gmail.com>
2015-04-20 21:27:13 +0800

    加上token的校验

--- src/gsvr_handler.erl
+++ src/gsvr_handler.erl
@@ -13,8 +13,6 @@ init(_Transport, Req, []) ->
 
 handle(Req, State) ->
     {Method, Req} = cowboy_req:method(Req),
-    %?TRACE_VAR(Method),
-    ?DEBUG_MSG("***Method:~p", [Method]),
     {ok, Req1} = handle_req(Method, Req),
 	{ok, Req1, State}.
 
@@ -30,8 +28,6 @@ handle_req(_, Req) ->
 do_handle_get(undefined, Req) ->
 	cowboy_req:reply(400, [], <<"Missing account_id parameter.">>, Req);
 do_handle_get(AccountId, Req) ->
-    %?TRACE_VAR(AccountId),
-    ?DEBUG_MSG("****AccountId:~p", [AccountId]),
     Servers = get_account_server_infos(AccountId),
 	cowboy_req:reply(200, [
 		{<<"content-type">>, <<"application/json; charset=utf-8">>}
@@ -39,20 +35,24 @@ do_handle_get(AccountId, Req) ->
 
 do_handle_post(true, Req) ->
     {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
-    %?TRACE_VAR(JsonBin),
-    ?DEBUG_MSG("******JsonBin:~p", [JsonBin]),
     PostVals = jsx:decode(JsonBin, [return_maps]),
-    %?TRACE_VAR(PostVals),
-    ?DEBUG_MSG("*****PostVals:~p", [PostVals]),
-    #{<<"account_id">> := AccountId, <<"svr_id">> := SvrId} = PostVals,
-    NewInfo = maps:put(<<"time">>, util:unixtime(), maps:remove(<<"account_id">>, PostVals)),
-    Infos = get_account_server_infos(AccountId),
-    NewInfos = case util:mapskeyfind(SvrId, <<"svr_id">>, Infos) of
-                   false -> [NewInfo | Infos];
-                   _Info -> util:mapskeyreplace(SvrId, <<"svr_id">>, Infos, NewInfo)
-               end,
-    util:put_ets(account_servers, AccountId, NewInfos),
-	cowboy_req:reply(200, [{<<"content-type">>, <<"text/plain; charset=utf-8">>}], <<"ok">>, Req2);
+    {ok, GsvrToken} = application:get_env(gsvr, token),
+    case maps:get(<<"token">>, PostVals, undefined) of
+        undefined ->
+            cowboy_req:reply(400, [], <<"Missing token parameter.">>, Req);
+        GsvrToken ->
+            #{<<"account_id">> := AccountId, <<"svr_id">> := SvrId} = PostVals,
+            NewInfo = maps:put(<<"time">>, util:unixtime(), maps:remove(<<"account_id">>, PostVals)),
+            Infos = get_account_server_infos(AccountId),
+            NewInfos = case util:mapskeyfind(SvrId, <<"svr_id">>, Infos) of
+                           false -> [NewInfo | Infos];
+                           _Info -> util:mapskeyreplace(SvrId, <<"svr_id">>, Infos, NewInfo)
+                       end,
+            util:put_ets(account_servers, AccountId, NewInfos),
+            cowboy_req:reply(200, [{<<"content-type">>, <<"text/plain; charset=utf-8">>}], <<"ok">>, Req2);
+        _Other ->
+            cowboy_req:reply(400, [], <<"Token wrong.">>, Req)
+    end;
 do_handle_post(false, Req) ->
 	cowboy_req:reply(400, [], <<"Missing body.">>, Req).
 
